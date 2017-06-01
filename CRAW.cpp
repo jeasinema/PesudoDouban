c5e85a703d8ec6fdf286078f9127c6100ce0de44
Shiyuan07 <SarahWang07@Hotmail.com>
2017-06-01 17:27:14 +0800

    Update CRAW.cpp

--- CRAW.cpp
+++ CRAW.cpp
@@ -1,265 +1,89 @@
-#include "craw.h"
-#include <sys/time.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <curl/curl.h>
-#include <cstring>
-#include <cstdlib>
-#include <sstream>
-#include <regex>
-
-/*void CRAW::parseHostAndPagePath(const string url, string &hostUrl, string &pagePath){
-    hostUrl = url;
-    pagePath = "/";
-    int pos = hostUrl.find("http://");
-    if(pos != -1)
-        hostUrl = hostUrl.replace(pos, 7, "");
-    pos = hostUrl.find("https://");
-    if(pos != -1)
-        hostUrl = hostUrl.replace(pos, 8, "");
-    pos = hostUrl.find("/");
-    if(pos != -1){
-        pagePath = hostUrl.substr(pos);
-        hostUrl = hostUrl.substr(0,pos);
-    }
-}
-
-string CRAW::getPageContent(string url){
-    struct hostent *host;
-    string hostUrl, pagePath;
-    parseHostAndPagePath(url, hostUrl, pagePath);
-    if(0 ==(host = gethostbyname(hostUrl.c_str()))){
-        cout<<"get host by name error\n"<<endl;
-        exit(1);
-    }
-    
-    struct sockaddr_in pin;
-    int port = 80;
-    bzero(&pin, sizeof(pin));
-    pin.sin_family = AF_INET;
-    pin.sin_port = htons(port);
-    pin.sin_addr.s_addr = ((struct in_addr*)(host->h_addr))->s_addr;
-    
-    int isock;
-    if((isock = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-        cout<<"open socket error\n"<<endl;
-        exit(1);
-    }
-    
-    hostUrl += "https://";
-    string requestHeader;
-    requestHeader = "GET "+pagePath+" HTTP/1.1\r\n";
-    requestHeader += "Host: "+hostUrl+"\r\n";
-    //requestHeader += "Accept: *//*\r\n";
-    requestHeader += "User-Agent: Mozilla/4.0(compatible)\r\n";
-    requestHeader += "connection:Keep-Alive\r\n";
-    requestHeader += "\r\n";
-    
-    if(connect(isock, (const sockaddr *)&pin, sizeof(pin)) == -1){
-        cout<<"connect error\n"<<endl;
-        exit(1);
-    }
-    
-    if(send(isock, requestHeader.c_str(), requestHeader.size(), 0) == -1){
-        cout<<"send error\n"<<endl;
-        exit(1);
-    }
-    
-    struct timeval timeout = {1, 0};
-    setsockopt(isock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(struct timeval));
-    
-    char c;
-    bool flag = true;
-    while(recv(isock, &c, 1, 0) > 0){
-        if('\r' == c){
-            continue;
-        }else if('\n' == c){
-            if(false == flag)
-                break;
-            flag = false;
-        }else{
-            flag = true;
-        }
-    }
-    
-    int len, BUFFER_SIZE = 512;
-    char buffer[BUFFER_SIZE];
-    string pageContent = "";
-    while((len = recv(isock, buffer, BUFFER_SIZE - 1, 0)) > 0){
-        buffer[len] = '\0';
-        pageContent += buffer;
-    }
-    return pageContent;
-}*/
+#include "CRAW.hpp"
 extern "C" {
-void CRAW::init_string(struct string0 *s){
-    s->len = 0;
-    s->ptr = (char*)malloc(s->len+1);
-    if (s->ptr == NULL) {
-        fprintf(stderr, "malloc() failed\n");
-        exit(EXIT_FAILURE);
-    }
-    s->ptr[0] = '\0';
-}
-
-size_t CRAW::get_response(void *ptr, size_t size, size_t nmemb, struct string0 *s){
-    size_t new_len = s->len + size*nmemb;
-    s->ptr = (char*)realloc(s->ptr, new_len+1);
-    if (s->ptr == NULL) {
-        fprintf(stderr, "realloc() failed\n");
-        exit(EXIT_FAILURE);
-    }
-    memcpy(s->ptr+s->len, ptr, size*nmemb);
-    s->ptr[new_len] = '\0';
-    s->len = new_len;
-    
-    return size*nmemb;
-}
-
-string CRAW::getPageContent(string url){
-    CURL *curl;
-    CURLcode res;
-    
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    
-    curl = curl_easy_init();
-    string html;
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.43.0");
-        //#ifdef SKIP_PEER_VERIFICATION
-        /*
-         * If you want to connect to a site who isn't using a certificate that is
-         * signed by one of the certs in the CA bundle you have, you can skip the
-         * verification of the server's certificate. This makes the connection
-         * A LOT LESS SECURE.
-         *
-         * If you have a CA cert for the server stored someplace else than in the
-         * default bundle, then the CURLOPT_CAPATH option might come handy for
-         * you.
-         */
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-        //#endif
-        
-        //#ifdef SKIP_HOSTNAME_VERIFICATION
-        /*
-         * If the site you're connecting to uses a different host name that what
-         * they have mentioned in their server certificate's commonName (or
-         * subjectAltName) fields, libcurl will refuse to connect. You can skip
-         * this check, but this will make the connection less secure.
-         */
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-        //#endif
-        
-        // critic
-        struct string0 s;
-        init_string(&s);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, get_response);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
-        
-        /* Perform the request, res will get the return code */
-        res = curl_easy_perform(curl);
-        /* Check for errors */
-        if(res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
-        //printf("custmized print:\n");
-        //printf("%s\n", s.ptr);
-        html = s.ptr;
-        free(s.ptr);
-        //printf("custmized print end:\n");
-        
-        
-        /* always cleanup */
-        curl_easy_cleanup(curl);
-    }
-    
-    curl_global_cleanup();
-    return html;
-}
-}
-
-void CRAW::get_url(string keyword){
-    string url0 =  "https://www.douban.com/tag/" + keyword + "/movie";
-    string str;
-    string pattern = "(<dl>)(.|\\r|\\n)*?(</dl>)";
-    pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
-    regex r(pattern);
-    for(int i = 0; i < 300; i = i + 15)
-    {
-        stringstream stream;
-        stream<<i;
-        str=stream.str();
-        url0 = url0 + "?start=" + str;
-        string info = getPageContent(url0);
-        for(sregex_iterator it(info.begin(), info.end(), r), end; it != end; ++it){
-            string str = it->str();
-            deal_the_dl(str);
+    void CRAW::init_string(struct string0 *s){
+        s->len = 0;
+        s->ptr = (char*)malloc(s->len+1);
+        if (s->ptr == NULL) {
+            fprintf(stderr, "malloc() failed\n");
+            exit(EXIT_FAILURE);
         }
-
+        s->ptr[0] = '\0';
     }
-        
-}
-
-string CRAW::get_pattern(string page, string pattern){
-    pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
-    regex r(pattern);
-    sregex_iterator it(page.begin(), page.end(), r);
-    string str = it->str();
-    return str;
-}
-
-char* CRAW::turn_to_char(string str){
-    cout<<str.size()<<endl;
-    char *tmp = new char[str.size()+1];
-    cout<<str.c_str()<<endl;
-    memcpy(tmp, str.c_str(), str.size() + 1);
-    cout<<tmp<<endl;
-    return tmp;
-}
-
-char *CRAW::get_keyinfo(string str){
-    char *tmp = turn_to_char(str);
-    return get_keyinfo(tmp);
-}
-
-char *CRAW::get_keyinfo(char *src){  //获得><之间的信息
-    while(*src)
-    {
-        if(*src == '>')
-            break;
-        src ++;
-    }
-    src ++;
-    char *fp = new char[20];
-    char *it = fp;
-    while (*src) {
-        if(*src == '<')
-            break;
-        if ((*src != ' ') && (*src != '\n') && (*src != '\r')) { // 如果不是空格就复制
-            *it = *src;
-            it++;
+    
+    size_t CRAW::get_response(void *ptr, size_t size, size_t nmemb, struct string0 *s){
+        size_t new_len = s->len + size*nmemb;
+        s->ptr = (char*)realloc(s->ptr, new_len+1);
+        if (s->ptr == NULL) {
+            fprintf(stderr, "realloc() failed\n");
+            exit(EXIT_FAILURE);
         }
-        src++;
+        memcpy(s->ptr+s->len, ptr, size*nmemb);
+        s->ptr[new_len] = '\0';
+        s->len = new_len;
+        
+        return size*nmemb;
     }
-    *it = '\0' ; //封闭字符串
-    cout<<fp<<endl;
-    return fp;
-}
-
-
-void CRAW::deal_the_dl(string info){
-    string url0 = get_pattern(info, "http.*?from=tag_all");
-    url.push_back(turn_to_char(url0));
     
-    string name0 = get_pattern(info, "class=\"title\".*?</a>");
-    name.push_back(get_keyinfo(name0));
-    
-    string tag0 = get_pattern(info, "<div class=\"desc\">(.|\\r|\\n)*?</div>");
-    tag.push_back(get_keyinfo(tag0));
-    
-    string img0 = get_pattern(info, "http.*?jpg");
-    img.push_back(turn_to_char(img0));
-    
+    string CRAW::getPageContent(string url){
+        CURL *curl;
+        CURLcode res;
+        
+        curl_global_init(CURL_GLOBAL_DEFAULT);
+        
+        curl = curl_easy_init();
+        string html;
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.43.0");
+            //#ifdef SKIP_PEER_VERIFICATION
+            /*
+             * If you want to connect to a site who isn't using a certificate that is
+             * signed by one of the certs in the CA bundle you have, you can skip the
+             * verification of the server's certificate. This makes the connection
+             * A LOT LESS SECURE.
+             *
+             * If you have a CA cert for the server stored someplace else than in the
+             * default bundle, then the CURLOPT_CAPATH option might come handy for
+             * you.
+             */
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+            //#endif
+            
+            //#ifdef SKIP_HOSTNAME_VERIFICATION
+            /*
+             * If the site you're connecting to uses a different host name that what
+             * they have mentioned in their server certificate's commonName (or
+             * subjectAltName) fields, libcurl will refuse to connect. You can skip
+             * this check, but this will make the connection less secure.
+             */
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+            //#endif
+            
+            // critic
+            struct string0 s;
+            init_string(&s);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, get_response);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+            
+            /* Perform the request, res will get the return code */
+            res = curl_easy_perform(curl);
+            /* Check for errors */
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                        curl_easy_strerror(res));
+            //printf("custmized print:\n");
+            //printf("%s\n", s.ptr);
+            html = s.ptr;
+            free(s.ptr);
+            //printf("custmized print end:\n");
+            
+            
+            /* always cleanup */
+            curl_easy_cleanup(curl);
+        }
+        
+        curl_global_cleanup();
+        return html;
+    }
 }
 
